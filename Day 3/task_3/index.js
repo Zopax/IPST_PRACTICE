2918a16dc2ef4da332fe49ede585eba7a291e4e7
Ckirusha <cyrilzara@mail.ru>
2025-01-15 20:19:11 +0700

    Complete day 3, 3rd task

---
+++ Day 3/task_3/index.js
@@ -0,0 +1,18 @@
+;
+var festData = function () {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            var data = [
+                { "id": "123", "name": "Alice", "email": "email1@example.com" },
+                { "id": "234", "name": "Bob", "email": "email2@example.com" },
+                { "id": "345", "name": "Jilly", "email": "email3@example.com" },
+            ];
+            resolve(data);
+        }, 2000);
+    });
+};
+festData().then(function (users) {
+    console.log("Данные получены:\n", users);
+}).catch(function (error) {
+    console.error("Ошибка при загрузке данных: ", error);
+});
---
+++ Day 3/task_3/index.ts
@@ -0,0 +1,24 @@
+interface User {
+    id: string;
+    name: string;
+    email: string;
+};
+
+const festData = (): Promise<User[]> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const data: User[] = [
+                {"id": "123", "name": "Alice", "email": "email1@example.com"},
+                {"id": "234", "name": "Bob", "email": "email2@example.com"},
+                {"id": "345", "name": "Jilly", "email": "email3@example.com"},
+            ];
+            resolve(data);
+        }, 2000);
+    });
+}
+
+festData().then((users) => {
+    console.log("Данные получены:\n", users);
+}).catch((error) => {
+    console.error("Ошибка при загрузке данных: ", error);
+});
